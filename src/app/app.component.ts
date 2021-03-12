61c516bd9263c166de7f63e251af3dea0c2dc544
Surendra <surendravikram85@gmail.com>
2021-03-12 15:03:22 +0530

    This is a new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { InternServiceService } from './intern-service.service';
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserServiceService } from './user-service.service';
 import * as _ from 'lodash';
 
@@ -8,59 +8,53 @@ import * as _ from 'lodash';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit  {
-  btnflag:boolean=false;
-  functionData:any;
+export class AppComponent implements OnInit {
+  btnflag: boolean = false;
+  functionData: any;
   title = 'Project2';
-  product:any=[];
-  formbtn:boolean;
-  image=String;
+  product: any = [];
+  formbtn: boolean;
+  image = String;
   // formbtn1:boolean;
- 
-  constructor(private service2:UserServiceService){ }
+
+  constructor(private service2: UserServiceService) { }
   ngOnInit() {
-    this.service2.name.subscribe(n=>{
-      this.product=n;
-      this.btnflag=true;
+    this.service2.name.subscribe(n => {
+      this.product = n;
+      this.btnflag = true;
     })
   }
-  
-  addData(value){
-    this.formbtn=false;
+
+  addData(value) {
+    this.formbtn = false;
     this.service2.addData.next(value);
   }
   readFile(fileEvent: any) {
     const file = fileEvent.target.files[0];
-    this.image=file.name;
+    this.image = file.name;
     const maxSize = 20000000;
     const allowed_types = ['image/png', 'image/jpeg'];
-    if(file.size>maxSize){
+    if (file.size > maxSize) {
       alert("Maximum file size must be 20 MB")
       return;
     }
     if (!_.includes(allowed_types, file.type)) {
       alert('Only Images are allowed ( JPG | PNG )');
       return false;
-  }
-    console.log('size', file.size);
-    console.log('type', file.type);
-    console.log(file);
-    console.log(this.image);
-    console.log(fileEvent.target.files.length);
-    console.log(file.size);
-    
+    }
+
   }
 
-  childData(data){
-    this.formbtn=data;
+  childData(data) {
+    this.formbtn = data;
   }
 
-  closeForm(){
-    this.formbtn=false;
+  closeForm() {
+    this.formbtn = false;
   }
 
-  deleteData(){
-    this.btnflag=false;
+  deleteData() {
+    this.btnflag = false;
     delete this.product.id;
     delete this.product.title;
     delete this.product.userId;
